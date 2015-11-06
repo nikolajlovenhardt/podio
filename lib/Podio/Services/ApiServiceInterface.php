d24e990fcb030ac41ea04f2efad44e62a6ac2026
Nikolaj Petersen <nikolaj.lovenhardt@gmail.com>
2015-11-06 15:31:14 +0100

    Removed constants

--- lib/Podio/Services/ApiServiceInterface.php
+++ lib/Podio/Services/ApiServiceInterface.php
@@ -4,11 +4,6 @@ namespace nlp\Podio\Services;
 
 interface ApiServiceInterface
 {
-    const GET = 'GET';
-    const POST = 'POST';
-    const PUT = 'PUT';
-    const DELETE = 'DELETE';
-
     public function get($url, array $attributes = [], array $options = []);
 
     public function post($url, array $attributes = [], array $options = []);
